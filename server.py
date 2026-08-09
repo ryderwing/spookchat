975f5d6dd2aeb6aea25553d033348e8d2c827ffb
ryderwing <monkeydoodle600@gmail.com>
2026-08-08 18:52:20 -0700

    Update server.py

--- server.py
+++ server.py
@@ -21,8 +21,8 @@ DATABASE = os.environ.get(
 )
 
 OWNER_USERNAME = os.environ.get(
-    "",
-    ""
+    "SPOOKCHAT_OWNER_USERNAME",
+    "JAYDEN"
 )
 
 # IMPORTANT:
@@ -34,7 +34,7 @@ OWNER_USERNAME = os.environ.get(
 #
 # This uses SPOOKCHAT_OWNER_PASSWORD correctly.
 OWNER_PASSWORD = os.environ.get(
-    "",
+    "SPOOKCHAT_OWNER_PASSWORD",
     ""
 )
 
